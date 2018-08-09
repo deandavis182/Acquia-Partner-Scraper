f0fcfc966dbaeaa7f50b83183dd2f9bface2f586
dean <deandavis182@gmail.com>
2018-08-09 11:11:48 -0700

    N/a

--- Acquia_Partner_Scraper.py
+++ Acquia_Partner_Scraper.py
@@ -4,8 +4,10 @@ except ImportError:
     print("No module named 'googlesearch' found")
 
 # to search
+#query = "Geeksforgeeks"
 query = "site://https://www.acquia.com/partners/showcase/"
  
-for j in search(query, tld="co.in", num=10, stop=None, pause=2.0):
+for j in search(query, tld="co.in", num=10, stop=1, pause=2.0):
+#for j in search(query, tld="com", num=10, stop=None, pause=2.0):
     print(j)
 
